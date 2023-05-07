35331960ea21dcbe15fcb710b7b836ad01fd09ed
Wasid Ali <wasidali77@gmail.com>
2023-05-07 16:07:11 +0530

    error handling middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db');
 
 //load env vars
@@ -29,10 +30,10 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 //Mount Router
-app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/bootcamps', bootcamps);
 
 
-
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow));
